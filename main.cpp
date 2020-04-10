81c806095b7c57df9335867bd052d9844393ceaa
Fatih İver <fatih.iver@boun.edu.tr>
2020-04-10 23:57:44 +0300

    create results.txt file

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@
 #include <iomanip>
 #include <cmath>
 #include <algorithm>
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+double findPrimeNumbers(int M, int T, int S, int C, bool output){
 
     auto start = chrono::high_resolution_clock::now();
-
-    int M = stoi(argv[1]);
 
     int SL = sqrt(M);
 
@@ -20,7 +20,9 @@ int main(int argc, char *argv[]) {
 
     int N = M + 1;
 
-    int T = argc > 2 ? stoi(argv[2]) : 1;
+    omp_set_num_threads(T);
+
+    omp_set_schedule(static_cast<omp_sched_t>(S), C);
 
     vector<int> primes;
 
@@ -64,13 +66,12 @@ int main(int argc, char *argv[]) {
 
     //-----------------------------------------------------------------------------
 
-
-    #pragma omp parallel num_threads(T) default(shared) private(n)
+    #pragma omp parallel default(shared) private(n)
     {
 
         vector<int> primesT;
 
-        #pragma omp for schedule(auto)
+        #pragma omp for
         for(n = SU; n <= N; n+=2){
 
             int rem;
@@ -115,19 +116,94 @@ int main(int argc, char *argv[]) {
 
     sort(primes.begin(), primes.end());
 
-    /*
-    for(int i = 0; i < primes.size(); i++)
-        cout << primes[i] << " ";
-    cout << endl;
-     */
-
-    cout << "Greatest prime smaller than or equal to "<< M << " is " << primes.back() << endl;
-
     auto end = chrono::high_resolution_clock::now();
 
     double time_taken =chrono::duration_cast<chrono::nanoseconds>(end - start).count() *  1e-9;
 
-    cout << "Time taken by the program is " << fixed<< time_taken << setprecision(9) << " seconds" << endl;
+    if(output){
+        cout << endl << primes.size() << " prime numbers found in the interval [2," << M << "]." << endl;
+        cout << "Calculation time is " <<  fixed << time_taken << setprecision(9) << " seconds." << endl;
+        cout << "Check the created primes.txt file to see found prime numbers!" << endl;
+
+        ofstream outfile;
+
+        outfile.open("primes.txt");
+
+        for(int prime : primes)
+            outfile << prime << endl;
+
+        outfile.close();
+    }
+
+    return time_taken;
+
+}
+
+int main(int argc, char *argv[]) {
+
+    int M = stoi(argv[1]);
+
+    int T = argc > 2 ? stoi(argv[2]) : 1;
+
+    int S = argc > 3 ? stoi(argv[3]) : 4;
+
+    int C = argc > 4 ? stoi(argv[4]) : 0;
+
+    findPrimeNumbers(M, T, S, C, true);
+
+    cout << endl << "Waiting for result.txt file to be created!" << endl;
+
+    ofstream outfile;
+
+    outfile.open("results.txt");
+
+    cout << "M" << "," << "Openmp Loop Scheduling Method" << "," << "Chunk Size" << ",";
+    outfile << "M" << "," << "Openmp Loop Scheduling Method" << "," << "Chunk Size" << ",";
+
+
+    for(int t =1; t <= T; t*=2){
+        cout << "," << "T" << t;
+        outfile << "," << "T" << t;
+    }
+
+    for(int s = 2; s <= T; s*=2){
+        cout << "," << "S" << s;
+        outfile << "," << "S" << s;
+    }
+
+    cout << endl;
+    outfile << endl;
+
+    for(int s = 1; s <= 4; s++){
+
+        for(int c = 1; c < M; c *= 10){
+
+            cout << M << "," << s << "," << c;
+            outfile << M << "," << s << "," << c;
+
+            vector<double> times;
+
+            for(int t =1; t <= T; t*=2){
+                double time = findPrimeNumbers(M, t, s, c, false);
+                cout << "," << time;
+                outfile << "," << time;
+                times.push_back(time);
+            }
+
+            for(int i = 1; i < times.size(); i++){
+                cout << "," << times.at(0) / times.at(i);
+                outfile << "," << times.at(0) / times.at(i);
+            }
+
+            cout << endl;
+            outfile << endl;
+
+        }
+    }
+
+    outfile.close();
+
+    cout << "Check the created results.txt file to see calculation results!" << endl;
 
     return 0;
 
